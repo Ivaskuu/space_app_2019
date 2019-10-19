e74fec263d15c18e09439709023190e3c8f108df
ivaskuu <ivascuadrian9@gmail.com>
2019-10-19 11:31:28 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/marco_apollo/space_app_2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marco_apollo.space_app_2019
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:space_app_2019/pages/home/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SpaceApp 2019',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
