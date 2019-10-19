70e4ddc668f86c72b37a31c5d745916dafd14dc3
Gabritherake <gabriele.biffi@libero.it>
2019-10-20 00:23:30 +0200

    rimosso widget

--- lib/pages/report/report_fire.dart
+++ lib/pages/report/report_fire.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:space_app_2019/widgets/confirmationWidget.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ReportFire extends StatefulWidget {
@@ -36,7 +35,7 @@ class _ReportFireState extends State<ReportFire> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*body: _fireImage == null
+      body: _fireImage == null
           ? Center(
               child: CircularProgressIndicator(),
             )
@@ -304,8 +303,7 @@ class _ReportFireState extends State<ReportFire> {
                   ),
                 )
               ],
-            ),*/
-      body: ConfirmFireWidget(_fireImage),
+            ),
     );
   }
 }
