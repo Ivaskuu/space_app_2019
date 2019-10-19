0aba541d633bb49e14a1aa4bf53cefae096d0db5
Pizio Leonardo <pizio.leonardo@gmail.com>
2019-10-19 13:09:16 +0200

    Added the fcm support for notifications.
    Added the support for firestore to store the uuid to send the

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:space_app_2019/pages/home/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:space_app_2019/pages/loginPage/loginpage.dart';
+import 'package:space_app_2019/states/NotifyState.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      ChangeNotifierProvider(
+        builder: (context) => NotifyState()..getUUID(),
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final FirebaseMessaging _fcm = FirebaseMessaging();
   @override
   Widget build(BuildContext context) {
+    _fcm.configure(onMessage: (Map<String, dynamic> message) async {
+      print('onResume: $message');
+    }, onResume: (Map<String, dynamic> message) async {
+      print('onResume: $message');
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print('onLaunch: $message');
+    });
     return MaterialApp(
       title: 'SpaceApp 2019',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
